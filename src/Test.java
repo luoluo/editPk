07fed54e0a1bed276165fd8008e16223492ef99a
luoluo <luoluo1920@gmail.com>
2012-11-15 15:09:23 +0800

    实现了问题的复杂度升级，问题是可以设置长度的了

--- src/Test.java
+++ src/Test.java
@@ -1,57 +1,110 @@
 import java.util.*;
 import java.io.*;
+
+
 class EditPK {
-	private int _score, _wrong;
-	private int a, b;
+	private int  _wrong;
 	private String question, answer;
-	private String questions = "qwertyuiopasdfghjkl;'zxcvbnm,./";
-	static Random rand = new Random();
 	EditPK(){
 		this(3);
 	}
 	EditPK(int wrong){
 		_wrong = wrong;	
 	}
-	int gaming(){
+
+	int gaming(int _score){
 		while(_wrong > 0){
-			getAndCheckInput();
+			putOut();
+			boolean flag = getInAndCheck();
+			if(!flag)
+				_wrong--;
+			else
+				_score++;
 		}
-		return _score;	//实现输出字符并匹配；	
+		return _score;
 	}
-	void getAndCheckInput(){
-		putOut();
-		boolean flag = getIn();
-		if(!flag)
-			_wrong--;
-		else
-			_score++;
-	}
+
 	void putOut(){
-		a = rand.nextInt(questions.length());
-//		b = rand.nextInt(100);
-		question = "" + questions.charAt(a);
+		question = gentrateQuestion();
 		answer = question; 
 		System.out.println(question);		
 	}
-	boolean getIn(){
+	boolean getInAndCheck(){
 		Scanner in = new Scanner(System.in);
 			String s;
 			s =  in.nextLine();
 			return s.equals(answer);
 	}	
+	String gentrateQuestion(){
+		//可以在此处实现一个多台的类来实现多种不同难度的输出：单个、多个 以及时间控制；
+		return new LevelQuestion().generate();	
+	}
 		
 }
+
+
+class RandQues{
+	static Random rand = new Random();
+	String questions = "qwertyuiopasdfghjkl;'zxcvbnm,./";
+	String nextChar(){
+		int	r = rand.nextInt(questions.length());
+		return "" + questions.charAt(r);	
+	}
+	
+}
+
+	
+interface Question{
+	static RandQues r = new RandQues();
+	String generate();	
+}
+class SimpleQuestion implements Question{
+	public	String generate(){
+		return r.nextChar();
+	}
+}
+class ComplexQuestion implements Question{
+	public String generate(){
+		return r.nextChar() + r.nextChar();
+	}
+		
+}
+class MoreComplexQuestion implements Question{
+	public String generate(){
+		return r.nextChar() + r.nextChar() + r.nextChar();	
+	}	
+}
+class LevelQuestion implements Question{
+	int _level;
+	LevelQuestion(){
+		_level = 3;	
+	}
+	LevelQuestion(int level){
+		_level = level;	
+	}
+	public String generate(){
+		String s = "";
+		for(int i = 0; i < _level; i++)
+			s += r.nextChar();
+		return s;
+	}	
+}
+
+
 class Player {
 	private	int _score;
 	void play(){
 		EditPK g = new EditPK();
-		_score = g.gaming();
+		_score = g.gaming(0);
 		showScore();
 	}
 	void showScore(){
 		System.out.println("Your score is: " + _score + "\n");	
 	}	
 }
+
+
+
 public class Test {
 	public static void main(String [] args){
 		Player p = new Player();
